cf75329d7f57a4701c77bcf3a68018db25715ea0
Morozov Anton <antonmorozoff64@gmail.com>
2022-08-09 22:33:12 +0200

    update: delete frontend

--- frontend/src/app/redux/index.ts
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import user from './reducers/user';
-import categories from './reducers/category'
-
-export default combineReducers({
-	user,
-	categories
-});
--- frontend/src/app/redux/reducers/expense.ts
+++
@@ -1,1 +0,0 @@
-import { AnyAction } from '@reduxjs/toolkit';
